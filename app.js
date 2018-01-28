e6f48005c0912583c5dc735037e4da0d8adb9bd6
mgh3326 <mgh3326@naver.com>
2018-01-28 18:33:47 +0900

    static dynamic update

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send('Hello home page!');
+});
+app.get('/dynamic', function (req, res) {
+    var lis = '';
+    for (var i = 0; i < 5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+            ${lis}
+        <ul>
+        ${time}
+    </body>
+    </html>`;
+    res.send(output);
+})
+app.get('/route', function (req, res) {
+    res.send('Hello Router, <img src="/route.png">');
+});
+app.get('/login', function (req, res) {
+    res.send('<h1>login please</h1>');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
